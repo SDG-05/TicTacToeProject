7d875eb3a18ea1738570a2367e2c356b022770b2
SDG-05 <rafiabdullah137@gmail.com>
2025-04-28 10:29:18 +0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        View login = new View();
+        login.LoginView();
+    }
+}
+
